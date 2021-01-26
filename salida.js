2a0e8554ad653cbc77ff74fde730f0ebb05d5092
Sebastian <jsruiz7411@misena.edu.co>
2021-01-26 15:48:42 -0500

    typescrip tutorial compleot

--- salida.js
+++ salida.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 console.log("Hello World");
 // Tipos 
 var myString = "Hola Mundo";
@@ -17,3 +30,61 @@ var strNumTpl;
 strNumTpl = ["hola", 5];
 strNumTpl = ["world", 280];
 // void undefined null
+//let myVoid:void = undefined
+//let myNull:null = null
+//let myUnDefined:undefined = undefined
+//document.write(typeof(myVoid))
+function getSuma(num1, num2) {
+    return num1 + num2;
+}
+var mySuma = function (num1, num2) {
+    if (typeof (num1 === 'string')) {
+        num1 = parseInt(num1);
+    }
+    if (typeof (num2 === 'string')) {
+        num2 = parseInt(num2);
+    }
+    return num1 + num2;
+};
+function showToDo(todo) {
+    console.log(todo.title + "." + todo.texto);
+}
+var myToDo = { title: 'carro', texto: 'Carro XD XD' };
+showToDo(myToDo);
+// Clases
+var User = /** @class */ (function () {
+    function User(name, email, edad) {
+        this.name = name;
+        this.email = email;
+        this.edad = edad;
+    }
+    User.prototype.registrar = function () {
+        console.log(this.name + " esta registrado");
+    };
+    User.prototype.mostrarEdad = function () {
+        return this.edad;
+    };
+    User.prototype.edadAnios = function () {
+        return this.edad + " Años";
+    };
+    User.prototype.pagado = function () {
+        return console.log(this.name + " Ha pagado su factura");
+    };
+    return User;
+}());
+var Juan = new User('Juan', 'ju@gmail.com', 23);
+console.log(Juan.edadAnios());
+var Miembros = /** @class */ (function (_super) {
+    __extends(Miembros, _super);
+    function Miembros(id, name, email, edad) {
+        var _this = _super.call(this, name, email, edad) || this;
+        _this.id = id;
+        return _this;
+    }
+    Miembros.prototype.pagado = function () {
+        _super.prototype.pagado.call(this);
+    };
+    return Miembros;
+}(User));
+var Jay = new Miembros(105, "Jay", "ja@email.com", 48);
+Jay.pagado();
--- salida.ts
+++ salida.ts
@@ -32,5 +32,131 @@ strNumTpl = ["world",280]
 
 // void undefined null
 
+//let myVoid:void = undefined
+//let myNull:null = null
+//let myUnDefined:undefined = undefined
 
 
+//document.write(typeof(myVoid))
+
+function getSuma(num1:number,num2:number):number{
+
+    return num1 + num2
+}
+
+let mySuma = function(num1:number|string,num2:number|string):number {
+
+if(typeof(num1 === 'string')){
+
+    num1 = parseInt(num1)
+
+}
+if(typeof(num2 === 'string')){
+
+    num2 = parseInt(num2)
+
+}
+
+return num1 + num2
+
+}
+/*
+function getName(firstName:string,lastName?:string):string{
+    if(lastName == undefined){
+        return firstName
+    }
+    else {
+    return firstName+" "+lastName;
+    }
+}
+
+document.write(getName("Juan"))*/
+
+/* Funciones sin retorno
+function myVoidFunction():void{
+
+
+}*/
+
+interface IToDo{
+    title: string,
+    texto:string
+}
+
+function showToDo(todo: IToDo){
+    console.log(`${todo.title}.${todo.texto}`)
+}
+
+let myToDo: IToDo = {title: 'carro', texto: 'Carro XD XD'}
+
+showToDo(myToDo)
+
+// Clases
+
+class User {
+
+    name: string
+   public email:string
+    protected edad:number
+
+    constructor(name:string,email:string,edad:number) {
+
+        this.name= name
+        this.email= email
+        this.edad = edad
+        
+        
+    }
+
+    registrar(){
+        console.log(`${this.name} esta registrado`)
+    }
+
+    mostrarEdad(){
+
+        return this.edad
+    }
+
+    edadAnios(){
+        return this.edad+" Años"
+    }
+
+    pagado(){
+        return console.log(`${this.name} Ha pagado su factura`)
+    }
+}
+
+var Juan = new User('Juan','ju@gmail.com',23)
+
+console.log(Juan.edadAnios())
+
+
+
+class Miembros extends User{
+
+    id: number;
+
+
+constructor(id,name,email,edad){
+
+super(name,email,edad);
+
+this.id = id
+
+}
+
+pagado(){
+    super.pagado()
+}
+
+
+}
+
+
+
+
+
+
+var Jay = new Miembros(105,"Jay","ja@email.com",48)
+
+Jay.pagado()
