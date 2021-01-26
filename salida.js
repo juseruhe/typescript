5e77ebd2748abfe230857b23cb3e24692a73defd
Sebastian <jsruiz7411@misena.edu.co>
2021-01-26 14:38:35 -0500

    Cambios

---
+++ salida.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+// Tipos 
+var myString = "Hola Mundo";
+myString = 22 + "";
+var myNumber = 22;
+var myBool = true;
+// Aceptar cualquier tipo de dato
+var myVar = "hello";
+myVar = false;
+// Arreglos
+var stringArray = ["Item1", "Item2", ""];
+var numberArray = [1, 2, 3];
+var booleanArray = [true, false, true];
+var anyArray = [1, 2, true, "hello", [], {}];
+//tuple
+var strNumTpl;
+strNumTpl = ["hola", 5];
+strNumTpl = ["world", 280];
+// void undefined null
---
+++ salida.ts
@@ -0,0 +1,36 @@
+console.log("Hello World")
+
+
+// Tipos 
+
+var myString: string= "Hola Mundo"
+myString = 22+""
+
+
+
+var myNumber:number= 22
+
+var myBool: boolean = true
+
+// Aceptar cualquier tipo de dato
+var myVar:any = "hello"
+myVar = false
+
+// Arreglos
+
+var stringArray:string[] = ["Item1","Item2",""]
+var numberArray:number[] = [1,2,3]
+var booleanArray:boolean[]= [true,false,true]
+var anyArray:any[] = [1,2,true,"hello",[],{}]
+
+
+//tuple
+
+var strNumTpl:[string,number]
+strNumTpl = ["hola",5]
+strNumTpl = ["world",280]
+
+// void undefined null
+
+
+
